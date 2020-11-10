05c0f72f353d4b364df3b1af82a776d2fe647392
tashi <tashafmukhtar@gmail.com>
2020-11-10 16:00:27 +0400

    final

--- app/src/main/java/com/tashi/aboutme/MainActivity.kt
+++ app/src/main/java/com/tashi/aboutme/MainActivity.kt
@@ -1,52 +1,47 @@
 package com.tashi.aboutme
 
 import android.content.Context
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.tashi.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var doneButton: Button
-    private  lateinit var userNickEditText: EditText
-    private lateinit var userNicknameTextView: TextView
+    private lateinit var mainActivityBinding: ActivityMainBinding
+    private val myName: MyName = MyName("Tashaf Mukhtar")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        initViews()
+        // setContentView(R.layout.activity_main)
+        mainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainActivityBinding.myName = myName
         settingUpOnClickListener();
-
-
 
 
     }
 
     private fun settingUpOnClickListener() {
-        doneButton.setOnClickListener {
-            val userNick : String = userNickEditText.text.toString()
-            userNicknameTextView.text = userNick
-            userNicknameTextView.visibility = View.VISIBLE
-            userNickEditText.visibility = View.INVISIBLE
-            // Hide the keyboard.
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
-            doneButton.visibility = View.INVISIBLE
+        mainActivityBinding.buttonDone.setOnClickListener {
+
+            mainActivityBinding.apply {
+                myName.nickName = mainActivityBinding.editTextTextPersonName.text.toString()
+                invalidateAll()
+                mainActivityBinding.textViewNick.text = myName.nickName
+
+                mainActivityBinding.textViewNick.visibility = View.VISIBLE
+                mainActivityBinding.editTextTextPersonName.visibility = View.INVISIBLE
+                // Hide the keyboard.
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(mainActivityBinding.buttonDone.windowToken, 0)
+                mainActivityBinding.buttonDone.visibility = View.INVISIBLE
+
+            }
         }
     }
 
-    private fun initViews() {
-        doneButton = findViewById(R.id.buttonDone)
-        userNickEditText = findViewById(R.id.editTextTextPersonName)
-        userNicknameTextView =  findViewById(R.id.textViewNick)
-    }
+
 }
---
+++ app/src/main/java/com/tashi/aboutme/MyName.kt
@@ -0,0 +1,4 @@
+package com.tashi.aboutme
+
+data class MyName(var name  : String = "", var nickName : String = "") {
+}
